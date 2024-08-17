46fa1de15887b09a5aeb2b2cbb25b152b86d7ddd
yogesh <101057204+yogii2000@users.noreply.github.com>
2024-08-17 10:15:31 -0700

    Update api.js

--- client/src/Utils/api.js
+++ client/src/Utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://adlift-backend.onrender.com/api';
 
 export const fetchProducts = async () => {
   try {
